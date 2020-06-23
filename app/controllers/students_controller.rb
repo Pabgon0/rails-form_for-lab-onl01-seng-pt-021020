4b822659f878fed333ae18ba691c320180e631c2
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-23 23:12:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   def show
-    @student = Student.find(params(:id))
+    @student = Student.find(params[:id])
   end
   
   def new
@@ -8,7 +8,7 @@ class StudentsController < ApplicationController
   end
   
   def edit
-    @student = Student.find(params(:id))
+    @student = Student.find(params[:id])
   end
   
   def create
@@ -18,7 +18,7 @@ class StudentsController < ApplicationController
   end
   
   def update
-    @student = Student.find(params(:id))
+    @student = Student.find(params[:id])
     @student.update(student_params)
     redirect_to student_path(@student)
   end
