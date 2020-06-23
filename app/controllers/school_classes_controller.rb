1d37d07909719c217acec205e9652e72c300d539
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-23 23:27:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -26,6 +26,6 @@ class SchoolClassesController < ApplicationController
   private
   
   def school_class_params(*args)
-    params.require(:school_class).permit
+    params.require(:school_class).permit(*args)
   end
 end
